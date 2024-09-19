e0e00090fc6d1a10fe9b6a2aa9a9395267e70786
AR Abdul Jobber <arjobbar2019@gmail.com>
2024-09-19 12:16:35 +0600

    first commit

---
+++ 20_1-Introduction-to-string-and-string-vs-array.js
@@ -0,0 +1,29 @@
+const country = 'Bangladesh';
+const division = "Khulna";
+const district = ` Chuadanga`; // backtick 
+const thana = new String ('Alamdanga');
+
+// console.log(typeof country );
+// console.log(thana);
+// console.log(typeof thana);
+
+const numbers = [54,98,78,21, 65];
+// console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers [0]);
+
+// numbers [1] = 1;
+// console.log(numbers);
+
+
+// String is immutable --> not changeable
+// array is mutable --> you can change the array elements
+
+const capital = 'Dhaka'
+// console.log(capital.length);
+// console.log(capital[1]);
+
+console.log(capital);
+// capital[1] = 'q';
+// console.log(capital);  
+
