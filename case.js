d541c2759a077b02a06fd685826242487e472c52
AR Abdul Jobber <arjobbar2019@gmail.com>
2024-09-23 15:14:35 +0600

    added to a case sensative in string

---
+++ case.js
@@ -0,0 +1,12 @@
+const school = 'RAJ UK Uttara Model School';
+// console.log(school);
+
+const subject = 'Chemistry';
+const book = 'chemistry';
+
+if(subject === book){
+    console.log('I am reading book ai ber porikkhai fatai felmu');
+}
+else{
+    console.log('hudai hudai prista ultai ar vat khawar jonno bosa thaki');
+}
