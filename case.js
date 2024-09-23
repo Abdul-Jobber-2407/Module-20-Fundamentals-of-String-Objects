b62204e21ef48977bb1cf7ecfb4e51106cdb4a1b
AR Abdul Jobber <arjobbar2019@gmail.com>
2024-09-23 16:18:34 +0600

    added to a case sensative

--- case.js
+++ case.js
@@ -1,12 +1,33 @@
 const school = 'RAJ UK Uttara Model School';
-// console.log(school);
+console.log(school);
+console.log(school.toLocaleLowerCase());
+console.log(school.toUpperCase());
 
+
+
+/************************************************************** */
+/* string is a  case sensative
+ * Uppercase : ABCDEFGH
+ * lowercase : abcdefgh */
 const subject = 'Chemistry';
 const book = 'chemistry';
 
-if(subject === book){
+if(subject.toLocaleLowerCase() === book.toLocaleLowerCase())
+
+    {
     console.log('I am reading book ai ber porikkhai fatai felmu');
 }
 else{
     console.log('hudai hudai prista ultai ar vat khawar jonno bosa thaki');
+}
+/*********************************************** */
+
+const drink = 'water';
+const liquid = ' water  ';
+
+if(drink.trim() === liquid.trim()){
+    console.log('pani er opor nam life');
+}
+else{
+    console.log('somudre pani aca khaite pari na');
 }
