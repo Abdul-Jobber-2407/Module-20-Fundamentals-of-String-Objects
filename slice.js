3439562128f37dfbfd4d264c524a556a894bf597
AR Abdul Jobber <arjobbar2019@gmail.com>
2024-09-30 12:42:19 +0600

    added to a slice

---
+++ slice.js
@@ -0,0 +1,27 @@
+/**
+const address = 'Mahomuda Anam Tonni';
+const part = address.slice(0,9);
+console.log(part);
+ */
+
+/** 
+const sentence = 'I am a good and hardworking person.';
+console.log(sentence.split (' '));
+console.log(sentence.split('a'));
+*/
+
+/**
+const friendsStr = 'Rahim, Kahim,dahim, lahim, fahim, sahim' ;
+const friends = friendsStr.split(',');
+console.log(friends);
+*/
+
+const realFriend = ['Rahim','Kahim', 'dahim', 'lahim',];
+console.log(realFriend);
+console.log(realFriend.join());
+console.log(realFriend.join('|'));
+console.log(realFriend.join('-'));
+
+
+
+
